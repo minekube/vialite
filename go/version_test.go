af1d4a4926af703112891e24e20ce4bb65f093f9
Robin Brämer <robin.braemer@web.de>
2026-06-22 21:42:57 +0200

    chore: prepare vialite v0.2.0 release

---
+++ go/version_test.go
@@ -0,0 +1,9 @@
+package vialite
+
+import "testing"
+
+func TestDefaultVersionTracksLatestRelease(t *testing.T) {
+	if DefaultVersion != "v0.2.0" {
+		t.Fatalf("DefaultVersion = %q, want v0.2.0", DefaultVersion)
+	}
+}
