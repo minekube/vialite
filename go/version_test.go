394c6bd7c0148f92ff3c5ead7f023f601e57fa1a
Robin <robin.braemer@web.de>
2026-06-23 09:04:53 +0200

    fix: allow velocity forwarding in native runtime

--- go/version_test.go
+++ go/version_test.go
@@ -3,7 +3,7 @@ package vialite
 import "testing"
 
 func TestDefaultVersionTracksLatestRelease(t *testing.T) {
-	if DefaultVersion != "v0.2.2" {
-		t.Fatalf("DefaultVersion = %q, want v0.2.2", DefaultVersion)
+	if DefaultVersion != "v0.2.3" {
+		t.Fatalf("DefaultVersion = %q, want v0.2.3", DefaultVersion)
 	}
 }
